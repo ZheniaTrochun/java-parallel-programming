e47e2d404c877e6c7ac9ce06341fd4a44948c2b6
Zhenia Trochun <zheniatrochun@gmail.com>
2018-10-29 23:57:28 +0300

    updated couchbase image

--- couchbase-document-loader/couchbase/init.sh
+++ couchbase-document-loader/couchbase/init.sh
@@ -13,7 +13,7 @@ sleep 15
 couchbase-cli cluster-init --cluster-username local --cluster-password localpass
 couchbase-cli user-manage -c localhost:8091 --username local --password localpass --set --rbac-username admin --rbac-password admin123 --auth-domain local --roles admin
 couchbase-cli bucket-create -c localhost:8091 --bucket-type couchbase --bucket-ramsize 500 -u admin -p admin123 --bucket=DataObject --wait
-cbq -u local -p localpass --script="create primary index on \`DataObject\`;"
+cbq -e http://localhost:8093 -u local -p localpass --script="create primary index on \`DataObject\`;"
 
 
 fg 1
