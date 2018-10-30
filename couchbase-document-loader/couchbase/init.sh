5376efa7f500c86247f0ea8689aa2badb96888e1
Yevhenii Trochun <Yevhenii.Trochun@playtech.com>
2018-10-30 13:23:15 +0200

    fixes for couchbase image

--- couchbase-document-loader/couchbase/init.sh
+++ couchbase-document-loader/couchbase/init.sh
@@ -7,13 +7,14 @@ echo "cool"
 
 /entrypoint.sh couchbase-server &
 
-sleep 15
+sleep 50
 
 
-couchbase-cli cluster-init --cluster-username local --cluster-password localpass
-couchbase-cli user-manage -c localhost:8091 --username local --password localpass --set --rbac-username admin --rbac-password admin123 --auth-domain local --roles admin
-couchbase-cli bucket-create -c localhost:8091 --bucket-type couchbase --bucket-ramsize 500 -u admin -p admin123 --bucket=DataObject --wait
-cbq -e http://localhost:8093 -u local -p localpass --script="create primary index on \`DataObject\`;"
+couchbase-cli cluster-init --cluster-username local --cluster-password localpass --wait
+couchbase-cli user-manage -c 0.0.0.0:8091 --username local --password localpass --set --rbac-username admin --rbac-password admin123 --auth-domain local --roles admin --wait
+couchbase-cli bucket-create -c 0.0.0.0:8091 --bucket-type couchbase --bucket-ramsize 500 -u admin -p admin123 --bucket=DataObject --wait
+# cbq -e 0.0.0.0:8093 -u admin -p admin123 --script="create primary index on \`DataObject\`;"
+cbq -u admin -p admin123 --script="create primary index on \`DataObject\`;"
 
 
 fg 1
