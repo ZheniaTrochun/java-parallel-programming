b7869f3797713de27ce210ac2db4aec8264ade29
Yevhenii Trochun <zheniatrochun@gmail.com>
2018-11-02 15:32:19 +0200

    fixed start script

--- couchbase-document-loader/start-all.bat
+++ couchbase-document-loader/start-all.bat
@@ -1,6 +1,6 @@
 
 docker build -t generation-tool .\tools
-docker run -v $(pwd)\java\reactive-data-service:/app/generated generation-tool
+docker run -v %cd%\java\reactive-data-service:/app/generated generation-tool
 echo "data generated"
 
 docker-compose build
